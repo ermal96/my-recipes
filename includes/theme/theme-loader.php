cb964ca854dc9f34d694486a847562be944d7170
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-12 15:18:26 +0200

    loader

--- includes/theme/theme-loader.php
+++ includes/theme/theme-loader.php
@@ -6,11 +6,9 @@
 function mr_theme_loader() {
 	?>
 	<div id="loader">
-		<div class="text-center">
-			<div class="spinner-border text-danger" role="status">
-				<span class="sr-only">Loading...</span>
-			</div>
-		</div>
+		
+		<h1><i class="fal fa-hat-chef"></i></h1>
+		
 	</div>
 	<?php
 }
