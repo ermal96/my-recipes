41f3b447d372a40b2dca65e3e557c55e6589ee38
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-13 16:24:09 +0200

    added seach console

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<meta name="google-site-verification" content="Etl65TadULD0kvXHIr_LsHR-AirM8H7JVI1tthSAq8o" />
 	<?php wp_head(); ?>
 </head>
 
