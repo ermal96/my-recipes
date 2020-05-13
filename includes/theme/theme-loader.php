5c24320d026937ecfcd9e731415ece81034b744d
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-13 16:03:57 +0200

    build

--- includes/theme/theme-loader.php
+++ includes/theme/theme-loader.php
@@ -7,7 +7,32 @@ function mr_theme_loader() {
 	?>
 	<div id="loader">
 		
-		<h1><i class="fal fa-hat-chef"></i></h1>
+	<div class="cssload-loading">
+	<div class="cssload-finger cssload-finger-1">
+		<div class="cssload-finger-item">
+			<span></span><i></i>
+		</div>
+	</div>
+	<div class="cssload-finger cssload-finger-2">
+		<div class="cssload-finger-item">
+			<span></span><i></i>
+		</div>
+	</div>
+	<div class="cssload-finger cssload-finger-3">
+		<div class="cssload-finger-item">
+			<span></span><i></i>
+		</div>
+	</div>
+	<div class="cssload-finger cssload-finger-4">
+		<div class="cssload-finger-item">
+			<span></span><i></i>
+		</div>
+	</div>
+	<div class="cssload-last-finger">
+		<div class="cssload-last-finger-item"><i></i></div>
+	</div>
+</div>
+
 		
 	</div>
 	<?php
