ea0c6aaadf3f9c1ffb0f8e7066d7eb327b06673d
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-13 16:06:49 +0200

    build

--- includes/theme/theme-loader.php
+++ includes/theme/theme-loader.php
@@ -7,32 +7,7 @@ function mr_theme_loader() {
 	?>
 	<div id="loader">
 		
-	<div class="cssload-loading">
-	<div class="cssload-finger cssload-finger-1">
-		<div class="cssload-finger-item">
-			<span></span><i></i>
-		</div>
-	</div>
-	<div class="cssload-finger cssload-finger-2">
-		<div class="cssload-finger-item">
-			<span></span><i></i>
-		</div>
-	</div>
-	<div class="cssload-finger cssload-finger-3">
-		<div class="cssload-finger-item">
-			<span></span><i></i>
-		</div>
-	</div>
-	<div class="cssload-finger cssload-finger-4">
-		<div class="cssload-finger-item">
-			<span></span><i></i>
-		</div>
-	</div>
-	<div class="cssload-last-finger">
-		<div class="cssload-last-finger-item"><i></i></div>
-	</div>
-</div>
-
+	
 		
 	</div>
 	<?php
