7bfd1b7178fae823cc5e3e9b81e82c6ef00be0f4
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-13 16:47:49 +0200

    allow webp upload

--- includes/theme/supports.php
+++ includes/theme/supports.php
@@ -42,7 +42,8 @@ if ( ! function_exists( 'my_recipes_setup' ) ) :
 			)
 		);
 
-		/*<?php mr_theme_loader(); ?>
+		/*
+		<?php mr_theme_loader(); ?>
 		 * Switch default core markup for search form, comment form, and comments
 		 * to output valid HTML5.
 		 */
@@ -92,3 +93,42 @@ function my_recipes_content_width() {
 }
 
 add_action( 'after_setup_theme', 'my_recipes_content_width', 0 );
+
+
+/**
+ * Sets the extension and mime type for .webp files.
+ *
+ * @param array  $wp_check_filetype_and_ext File data array containing 'ext', 'type', and
+ *                                          'proper_filename' keys.
+ * @param string $file                      Full path to the file.
+ * @param string $filename                  The name of the file (may differ from $file due to
+ *                                          $file being in a tmp directory).
+ * @param array  $mimes                     Key is the file extension with value as the mime type.
+ */
+add_filter( 'wp_check_filetype_and_ext', 'wpse_file_and_ext_webp', 10, 4 );
+function wpse_file_and_ext_webp( $types, $file, $filename, $mimes ) {
+	if ( false !== strpos( $filename, '.webp' ) ) {
+		$types['ext']  = 'webp';
+		$types['type'] = 'image/webp';
+	}
+
+	return $types;
+}
+
+/**
+ * Adds webp filetype to allowed mimes
+ *
+ * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/upload_mimes
+ *
+ * @param array $mimes Mime types keyed by the file extension regex corresponding to
+ *                     those types. 'swf' and 'exe' removed from full list. 'htm|html' also
+ *                     removed depending on '$user' capabilities.
+ *
+ * @return array
+ */
+add_filter( 'upload_mimes', 'wpse_mime_types_webp' );
+function wpse_mime_types_webp( $mimes ) {
+	$mimes['webp'] = 'image/webp';
+
+	return $mimes;
+}
